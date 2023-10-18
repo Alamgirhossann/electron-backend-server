7fa6d2202514b79962616d3918b3f503e29d4dde
Alamgirhossann <alamgirhossann@gmail.com>
2023-10-18 07:15:27 +0600

    updated

--- src/app/modules/generalUser/generalUser.controller.ts
+++ src/app/modules/generalUser/generalUser.controller.ts
@@ -1,83 +1,84 @@
-import { Request, Response } from 'express'
-import httpStatus from 'http-status'
-import { pagination } from '../../../constants/pagination'
-import catchAsync from '../../../shared/catchAsync'
-import pick from '../../../shared/pick'
-import sendResponse from '../../../shared/sendResponse'
-import { generalUserFilterableFields } from './generalUser.constant'
-import { GeneralUserService } from './generalUser.service'
-import { IGeneralUser } from './generalUser.interface'
+import { Request, Response } from "express";
+import httpStatus from "http-status";
+import { pagination } from "../../../constants/pagination";
+import catchAsync from "../../../shared/catchAsync";
+import pick from "../../../shared/pick";
+import sendResponse from "../../../shared/sendResponse";
+import { generalUserFilterableFields } from "./generalUser.constant";
+import { GeneralUserService } from "./generalUser.service";
+import { IGeneralUser } from "./generalUser.interface";
 
 const getAllGeneralUser = catchAsync(async (req: Request, res: Response) => {
-  const filters = pick(req.query, generalUserFilterableFields)
-  const paginationOptions = pick(req.query, pagination)
+  const filters = pick(req.query, generalUserFilterableFields);
+  const paginationOptions = pick(req.query, pagination);
 
   const result = await GeneralUserService.getAllGeneralUser(
     filters,
     paginationOptions
-  )
+  );
 
   sendResponse<IGeneralUser[]>(res, {
     statusCode: httpStatus.OK,
     success: true,
-    message: 'User retrieved successfully !',
+    message: "User retrieved successfully !",
     meta: result.meta,
     data: result.data,
-  })
-})
+  });
+});
 
 const createGeneralUser = catchAsync(async (req: Request, res: Response) => {
-  const { generalUser, ...userData } = req.body
+  const { generalUser, ...userData } = req.body;
   const result = await GeneralUserService.createGeneralUser(
     generalUser,
     userData
-  )
+  );
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
-    message: 'User created successfully',
+    message: "User created successfully",
     data: result,
-  })
-})
+  });
+});
 
 const getSingleGeneralUser = catchAsync(async (req: Request, res: Response) => {
-  const id = req.params.id
-  const result = await GeneralUserService.getSingleGeneralUser(id)
+  const id = req.params.id;
+  const result = await GeneralUserService.getSingleGeneralUser(id);
 
   sendResponse<IGeneralUser>(res, {
     statusCode: httpStatus.OK,
     success: true,
-    message: 'User retrieved successfully !',
+    message: "User retrieved successfully !",
     data: result,
-  })
-})
+  });
+});
 
 const updateGeneralUser = catchAsync(async (req: Request, res: Response) => {
-  const id = req.params.id
-  const updatedData = req.body
+  const id = req.params.id;
+  const updatedData = req.body;
+  console.log(id, updatedData);
 
-  const result = await GeneralUserService.updateGeneralUser(id, updatedData)
+  const result = await GeneralUserService.updateGeneralUser(id, updatedData);
 
   sendResponse<IGeneralUser>(res, {
     statusCode: httpStatus.OK,
     success: true,
-    message: 'User updated successfully !',
+    message: "User updated successfully !",
     data: result,
-  })
-})
+  });
+});
 const deleteGeneralUser = catchAsync(async (req: Request, res: Response) => {
-  const id = req.params.id
+  const id = req.params.id;
 
-  const result = await GeneralUserService.deleteGeneralUser(id)
+  const result = await GeneralUserService.deleteGeneralUser(id);
 
   sendResponse<IGeneralUser>(res, {
     statusCode: httpStatus.OK,
     success: true,
-    message: 'User deleted successfully !',
+    message: "User deleted successfully !",
     data: result,
-  })
-})
+  });
+});
 
 export const GeneralUserController = {
   getAllGeneralUser,
@@ -85,4 +86,4 @@ export const GeneralUserController = {
   updateGeneralUser,
   deleteGeneralUser,
   createGeneralUser,
-}
+};
--- src/app/modules/generalUser/generalUser.service.ts
+++ src/app/modules/generalUser/generalUser.service.ts
@@ -1,15 +1,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import httpStatus from 'http-status'
-import mongoose, { SortOrder } from 'mongoose'
-import ApiError from '../../../errors/ApiError'
-import { paginationHelpers } from '../../../helpers/paginationHelper'
-import { IPaginationOptions } from '../../../interfaces/pagination'
-import { User } from '../user/user.model'
-import { IGenericResponse } from '../../../interfaces/error'
-import { IGeneralUser, IGeneralUserFilters } from './generalUser.interface'
-import { generalUserSearchableFields } from './generalUser.constant'
-import { GeneralUser } from './generalUser.model'
-import { IUser } from '../user/user.interface'
+import httpStatus from "http-status";
+import mongoose, { SortOrder } from "mongoose";
+import ApiError from "../../../errors/ApiError";
+import { paginationHelpers } from "../../../helpers/paginationHelper";
+import { IPaginationOptions } from "../../../interfaces/pagination";
+import { User } from "../user/user.model";
+import { IGenericResponse } from "../../../interfaces/error";
+import { IGeneralUser, IGeneralUserFilters } from "./generalUser.interface";
+import { generalUserSearchableFields } from "./generalUser.constant";
+import { GeneralUser } from "./generalUser.model";
+import { IUser } from "../user/user.interface";
 
 const createGeneralUser = async (
   generalUser: IGeneralUser,
@@ -22,68 +22,68 @@ const createGeneralUser = async (
   //   }
 
   // set role
-  user.role = 'user'
+  user.role = "user";
 
   // generate faculty id
-  let newUserAllData = null
-  const session = await mongoose.startSession()
+  let newUserAllData = null;
+  const session = await mongoose.startSession();
   try {
-    session.startTransaction()
+    session.startTransaction();
 
-    user.id = generalUser.email
-    generalUser.id = generalUser.email
+    user.id = generalUser.email;
+    generalUser.id = generalUser.email;
 
-    const newAdmin = await GeneralUser.create([generalUser], { session })
+    const newAdmin = await GeneralUser.create([generalUser], { session });
 
     if (!newAdmin.length) {
-      throw new ApiError(httpStatus.BAD_REQUEST, 'Failed to create user ')
+      throw new ApiError(httpStatus.BAD_REQUEST, "Failed to create user ");
     }
 
-    user.generalUser = newAdmin[0]._id
+    user.generalUser = newAdmin[0]._id;
 
-    const newUser = await User.create([user], { session })
+    const newUser = await User.create([user], { session });
 
     if (!newUser.length) {
-      throw new ApiError(httpStatus.BAD_REQUEST, 'Failed to create user')
+      throw new ApiError(httpStatus.BAD_REQUEST, "Failed to create user");
     }
-    newUserAllData = newUser[0]
+    newUserAllData = newUser[0];
 
-    await session.commitTransaction()
-    await session.endSession()
+    await session.commitTransaction();
+    await session.endSession();
   } catch (error) {
-    await session.abortTransaction()
-    await session.endSession()
-    throw error
+    await session.abortTransaction();
+    await session.endSession();
+    throw error;
   }
 
   if (newUserAllData) {
     newUserAllData = await User.findOne({ id: newUserAllData.id }).populate({
-      path: 'generalUser',
-    })
+      path: "generalUser",
+    });
   }
 
-  return newUserAllData
-}
+  return newUserAllData;
+};
 
 const getAllGeneralUser = async (
   filters: IGeneralUserFilters,
   paginationOptions: IPaginationOptions
 ): Promise<IGenericResponse<IGeneralUser[]>> => {
-  const { searchTerm, ...filtersData } = filters
+  const { searchTerm, ...filtersData } = filters;
   const { page, limit, skip, sortBy, sortOrder } =
-    paginationHelpers.calculatePagination(paginationOptions)
+    paginationHelpers.calculatePagination(paginationOptions);
 
-  const andConditions = []
+  const andConditions = [];
 
   if (searchTerm) {
     andConditions.push({
-      $or: generalUserSearchableFields.map(field => ({
+      $or: generalUserSearchableFields.map((field) => ({
         [field]: {
           $regex: searchTerm,
-          $options: 'i',
+          $options: "i",
         },
       })),
-    })
+    });
   }
 
   if (Object.keys(filtersData).length) {
@@ -91,23 +91,23 @@ const getAllGeneralUser = async (
       $and: Object.entries(filtersData).map(([field, value]) => ({
         [field]: value,
       })),
-    })
+    });
   }
 
-  const sortConditions: { [key: string]: SortOrder } = {}
+  const sortConditions: { [key: string]: SortOrder } = {};
 
   if (sortBy && sortOrder) {
-    sortConditions[sortBy] = sortOrder
+    sortConditions[sortBy] = sortOrder;
   }
   const whereConditions =
-    andConditions.length > 0 ? { $and: andConditions } : {}
+    andConditions.length > 0 ? { $and: andConditions } : {};
 
   const result = await GeneralUser.find(whereConditions)
     .sort(sortConditions)
     .skip(skip)
-    .limit(limit)
+    .limit(limit);
 
-  const total = await GeneralUser.countDocuments(whereConditions)
+  const total = await GeneralUser.countDocuments(whereConditions);
 
   return {
     meta: {
@@ -116,75 +116,30 @@ const getAllGeneralUser = async (
       total,
     },
     data: result,
-  }
-}
+  };
+};
 
 const getSingleGeneralUser = async (
   id: string
 ): Promise<IGeneralUser | null> => {
-  const result = await GeneralUser.findById(id)
-  return result
-}
+  const result = await GeneralUser.findById(id);
+  return result;
+};
 
 const updateGeneralUser = async (
   id: string,
   payload: Partial<IGeneralUser>
 ): Promise<IGeneralUser | null> => {
-  const isExist = await GeneralUser.findOne({ id })
-
-  if (!isExist) {
-    throw new ApiError(httpStatus.NOT_FOUND, 'User not found !')
-  }
-
-  const { name, ...adminData } = payload
-
-  const updatedStudentData: Partial<IGeneralUser> = { ...adminData }
-
-  if (name && Object.keys(name).length > 0) {
-    Object.keys(name).forEach(key => {
-      const nameKey = `name.${key}` as keyof Partial<IGeneralUser>
-      ;(updatedStudentData as any)[nameKey] = name[key as keyof typeof name]
-    })
-  }
-
-  const result = await GeneralUser.findOneAndUpdate(
-    { id },
-    updatedStudentData,
-    {
-      new: true,
-    }
-  )
-  return result
-}
+  const result = await GeneralUser.findOneAndUpdate({ _id: id }, payload, {
+    new: true,
+  });
+  return result;
+};
 
 const deleteGeneralUser = async (id: string): Promise<IGeneralUser | null> => {
-  // check if the faculty is exist
-  const isExist = await GeneralUser.findOne({ id })
-
-  if (!isExist) {
-    throw new ApiError(httpStatus.NOT_FOUND, 'User not found !')
-  }
-
-  const session = await mongoose.startSession()
-
-  try {
-    session.startTransaction()
-    //delete student first
-    const student = await GeneralUser.findOneAndDelete({ id }, { session })
-    if (!student) {
-      throw new ApiError(404, 'Failed to delete user')
-    }
-    //delete user
-    await User.deleteOne({ id })
-    session.commitTransaction()
-    session.endSession()
-
-    return student
-  } catch (error) {
-    session.abortTransaction()
-    throw error
-  }
-}
+  const result = await GeneralUser.findByIdAndDelete(id);
+  return result;
+};
 
 export const GeneralUserService = {
   getAllGeneralUser,
@@ -192,4 +147,4 @@ export const GeneralUserService = {
   updateGeneralUser,
   deleteGeneralUser,
   createGeneralUser,
-}
+};
